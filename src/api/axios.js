ac7906dc3ad7ffb43f7b5c5430e1b5e015e4a245
ou786 <osamamohammed.ussaid@dentistryautomation.com>
2025-12-05 22:06:07 +0530

    updated axios baseurl

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://yusra-server.onrender.com/",
+  baseURL: "https://yusra-server.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
